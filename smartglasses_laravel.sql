74e7ffedc5c82a07c6c5b8110d9644172d3dc8c1
cindyka septriani <cindykaseptriani29@gmail.com>
2023-11-02 19:18:00 +0800

    first commit

---
+++ smartglasses_laravel.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 17, 2022 at 04:05 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_laravel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `disc`
+--
+-- Error reading structure for table db_laravel.disc: #1932 - Table &#039;db_laravel.disc&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_laravel.disc: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_laravel`.`disc`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+-- Error reading structure for table db_laravel.failed_jobs: #1932 - Table &#039;db_laravel.failed_jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_laravel.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_laravel`.`failed_jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table db_laravel.migrations: #1932 - Table &#039;db_laravel.migrations&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_laravel.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_laravel`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+-- Error reading structure for table db_laravel.password_resets: #1932 - Table &#039;db_laravel.password_resets&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_laravel.password_resets: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_laravel`.`password_resets`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+-- Error reading structure for table db_laravel.personal_access_tokens: #1932 - Table &#039;db_laravel.personal_access_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_laravel.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_laravel`.`personal_access_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rate`
+--
+-- Error reading structure for table db_laravel.rate: #1932 - Table &#039;db_laravel.rate&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_laravel.rate: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_laravel`.`rate`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table db_laravel.users: #1932 - Table &#039;db_laravel.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_laravel.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_laravel`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
